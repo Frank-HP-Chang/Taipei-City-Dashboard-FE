e32e59fb9f507c93e39635d197f2163b0d0e349d
Chang <frchang@deloitte.com.tw>
2023-11-19 13:04:17 +0800

    update build and tree

--- src/assets/configs/mapbox/mapConfig.js
+++ src/assets/configs/mapbox/mapConfig.js
@@ -199,6 +199,23 @@ export const maplayerCommonPaint = {
 		"fill-extrusion-opacity": 0.5,
 	},
 	heatmap: {
+		'heatmap-color': [
+			'interpolate',
+			['linear'],
+			['heatmap-density'],
+			0,
+			'rgba(33,102,172,0)',
+			0.2,
+			'rgb(103,169,207)',
+			0.4,
+			'rgb(209,229,240)',
+			0.6,
+			'rgb(253,219,199)',
+			0.8,
+			'rgb(239,138,98)',
+			1,
+			'rgb(178,24,43)'
+			],
 	},
 	fill: {
 		"fill-opacity": [
